f7398d27337296970511764925ab11a1c0afad74
kasinadhsarma <kasinadhsarma@gmail.com>
2024-07-12 07:23:15 +0000

    Fix E501 linting error by breaking long line into two lines in src/sensors.py

--- src/sensors.py
+++ src/sensors.py
@@ -30,10 +30,8 @@ class BirdRobotSensors:
         distances = []
         for obstacle in self.obstacles:
             distance = np.linalg.norm(position - obstacle)
-            angle = (
-                np.arctan2(obstacle[1] - position[1], obstacle[0] - position[0])
-                - np.deg2rad(orientation)
-            )
+            angle_to_obstacle = np.arctan2(obstacle[1] - position[1], obstacle[0] - position[0])
+            angle = angle_to_obstacle - np.deg2rad(orientation)
             if (
                 distance <= SENSOR_RANGE
                 and np.abs(np.rad2deg(angle)) <= SENSOR_ANGLE / 2
