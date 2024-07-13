f1b1ab089dacd9ce3fbb18670715b4d3d4d69646
kasinadhsarma <kasinadhsarma@gmail.com>
2024-07-13 00:24:30 +0000

    Remove unused import to fix F401 flake8 error

--- agents/train_agent.py
+++ agents/train_agent.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 from tf_agents.environments import tf_py_environment
-from tf_agents.policies import policy_saver
 from tf_agents.utils import common
 from tf_agents.agents.dqn import dqn_agent
 from tf_agents.networks import q_network
