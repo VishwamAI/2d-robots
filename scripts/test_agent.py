2296c477e9bd6df2b13a00586a843d9ceb9af0cb
kasinadhsarma <kasinadhsarma@gmail.com>
2024-07-13 01:24:54 +0000

    Debug sys.path for src module imports

--- scripts/test_agent.py
+++ scripts/test_agent.py
@@ -1,15 +1,15 @@
 import os
 import sys
 
-from tf_agents.environments import tf_py_environment
-from tf_agents.policies import py_tf_eager_policy
-from src.environment import BirdRobotEnvironment
-from config.config import POLICY_DIR
-
 # Append the src directory to the Python path
 sys.path.append(
     os.path.join(os.path.dirname(__file__), '..', 'src')
 )  # noqa: E402
+
+from tf_agents.environments import tf_py_environment
+from tf_agents.policies import py_tf_eager_policy
+from src.environment import BirdRobotEnvironment
+from config.config import POLICY_DIR
 
 # Create the environment
 eval_py_env = BirdRobotEnvironment()
